8f6e2d0b1a8d989621021e9ede91e5bfd9de5cb3
nguyenhieu <nguyenhieu2k123@gmail.com>
2021-11-08 14:48:44 +0700

    Change the api

--- src/test/getSongInf.java
+++ src/test/getSongInf.java
@@ -12,14 +12,13 @@ public class getSongInf {
 	public static void main (String[] args)  {
 		// TODO Auto-generated method stub
 		Gson gson = new Gson();
-		ArrayList<String> list = new ArrayList<String>();
-		HttpResponse<String> response = Unirest.get("https://unsa-unofficial-spotify-api.p.rapidapi.com/search?query=magenta%20riddim&count=20&type=tracks")
-				.header("x-rapidapi-host", "unsa-unofficial-spotify-api.p.rapidapi.com")
+		HttpResponse<String> response = Unirest.get("https://shazam-core.p.rapidapi.com/v1/tracks/search?query=Nơi này có anh")
+				.header("x-rapidapi-host", "shazam-core.p.rapidapi.com")
 				.header("x-rapidapi-key", "816000848fmshd35ce00429d6043p1296fdjsnea474485598e")
 				.asString();
 		String infString = response.getBody();
-		music music = gson.fromJson(infString, DTO.music.class);
-		System.out.println(music.results);
+//		music music = gson.fromJson(infString, DTO.music.class);
+		System.out.println(infString);
 //		for(int i = 0; i < music.results.size(); i++){
 //			System.out.println(music.results.get(i));
 //		}
