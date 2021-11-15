a4d61fe8e1ffefa753966d646c6db8c2a96b6dd4
nguyenhieu <nguyenhieu2k123@gmail.com>
2021-11-15 15:15:52 +0700

    Add GUI

--- src/GUI/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-Package này là khu vực chứa giao diện
--- src/test_HIeu/getSongInf.java
+++ src/test_HIeu/getSongInf.java
@@ -43,7 +43,6 @@ public class getSongInf {
 			}
 		}
 		System.out.println("=======================");
-		System.out.println("CHo Tuan");
 	}
 
 
