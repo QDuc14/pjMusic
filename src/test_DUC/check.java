32f46ab8e451866a0a0ca41c54d498b61d4f3ab9
TranHuy2608 <63567397+TranHuy2608@users.noreply.github.com>
2021-11-22 20:38:45 +0700

    Create check.java
    check kí tự

---
+++ src/test_DUC/check.java
@@ -0,0 +1,22 @@
+package test_DUC;
+
+package test;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class check {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        boolean b ;
+        do {
+            System.out.println("input txt :");
+            String txt = sc.nextLine();
+            Pattern p = Pattern.compile("^[A-Za-z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠ-ỹ ]+$");
+            Matcher m = p.matcher(txt);
+            b = m.find();
+            System.out.println(b);
+        }while(b==false);
+        System.out.println(b);
+    }
+}
